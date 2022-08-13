12d568b78176915288c7b5534c90657c1c828b89
hildelio <hildelio@gmail.com>
2022-08-13 01:02:20 -0300

    requisito

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,16 @@
-const fetchProducts = () => {
-  // seu código aqui
+const url = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
+const fetchProducts = async (prod) => {
+  try {
+    const urlProd = `${url}${prod}`;
+    const response = await fetch(urlProd);
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    return error;
+  }
 };
+// console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {
